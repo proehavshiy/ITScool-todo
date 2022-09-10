d1bede590b4f8911f6e4f4c55ad9c5d1fd557a60
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-09-10 18:54:43 +0300

    chore: init file structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import Header from '../Header/Header';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className='app'>
+      <Header />
+      App
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+import './Header.css';
+
+const Header: FC = () => {
+  return (
+    <header className="header">
+      <h1>todo list</h1>
+      {/* <Button
+        style='toggle-all'
+        handleClick={handleToggleAllBtn}
+      /> */}
+      {/* <Input /> */}
+    </header >
+  )
+}
+
+export default Header
